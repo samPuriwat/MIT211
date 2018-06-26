d26585098a8b44ff12e5b0dc713fb20aad8135eb
samPuriwat <sam.puriwat@gmail.com>
2018-06-26 11:40:12 +0700

    Hello World

---
+++ src/lab1/myCode.java
@@ -0,0 +1,29 @@
+package lab1;
+
+public class myCode {
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World.");
+        //name
+        System.out.println("Puriwat lertkrai");
+        //id
+        System.out.println("1111111111");
+        //group
+        System.out.println("MIT211");
+        //university
+        System.out.println("RMUTSV");
+
+
+
+
+
+
+    }//main
+
+
+
+
+
+}//class
