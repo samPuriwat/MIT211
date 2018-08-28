b78a50cd249893a35c562b48d5ebcb2915e50662
samPuriwat <sam.puriwat@gmail.com>
2018-08-28 10:58:59 +0700

    Lab 4 : Methods Overload

---
+++ src/lab4/ex3.java
@@ -0,0 +1,20 @@
+package lab4;
+
+public class ex3 {
+
+    public static void main(String[] args) {
+
+
+        System.out.println(divide(10.0,5));
+
+    }//main
+    //Overloading Methods
+    public static int divide(int x,int y){
+        return x / y;
+    }
+    public static double divide(double x,double y){
+        return x / y;
+    }
+
+
+}//class
