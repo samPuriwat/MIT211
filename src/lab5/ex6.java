947c6939b8629b0a229e0ebc1844b5cb91037f62
samPuriwat <sam.puriwat@gmail.com>
2018-09-18 11:20:45 +0700

    Lab 5 : StringTokenizer

--- src/lab5/ex6.java
+++ src/lab5/ex6.java
@@ -1,5 +1,7 @@
 package lab5;
 
+import java.util.StringTokenizer;
+
 public class ex6 {
     public static void main(String[] args) {
         //StringBuffer
@@ -17,6 +19,12 @@ public class ex6 {
         System.out.println(myStr2);
         myStr2.reverse();
         System.out.println(myStr2);
+
+        //StringTokenizer
+        StringTokenizer myStr3 = new StringTokenizer("Hello, my name is sam");
+        int count = myStr3.countTokens();
+        System.out.println(count);
+
 
 
 
