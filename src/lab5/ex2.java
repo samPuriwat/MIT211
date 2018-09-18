ddce6d27282e608f865ce5747f7a93849b1b2d33
samPuriwat <sam.puriwat@gmail.com>
2018-09-18 08:47:11 +0700

    Lab 5 : exercises with Array

--- src/lab5/ex2.java
+++ src/lab5/ex2.java
@@ -5,6 +5,7 @@ package lab5;
 //3. create method to find average value
 //4. create method to sort data in array  (low-high)****
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ex2 {
@@ -20,16 +21,52 @@ public class ex2 {
             System.out.print("Enter integer "+(i+1)+":");
             num[i] = scanner.nextInt();
         }
-        //output
-//        System.out.println("Data in array following: ");
-//        for (int i = 0; i < num.length; i++) {
-//                System.out.print(num[i]+"  ");
-//        }
-//        System.out.println();
-//
         showData(num);
+        findMax(num);
+        findMin(num);
+        findAverage(num);
+        sortData(num);
 
     }//main
+
+    private static void sortData(int[] num) {
+        Arrays.sort(num);
+        System.out.println();
+        showData(num);
+
+    }
+
+    private static void findAverage(int[] num) {
+        int sum = 0;
+        for (int i = 0; i < num.length; i++) {
+            sum += num[i];  // sum = sum+num[i];
+
+        }//loop
+        System.out.println("The average value is "+(sum/num.length));
+
+    }
+
+    private static void findMax(int[] num) {
+        int max = num[0];
+        for (int i = 0; i < num.length; i++) {
+            if (max <= num[i]) {
+                max = num[i];
+            }
+        }//loop
+        System.out.println();
+        System.out.println("The maximum value is " + max);
+    }
+    private static void findMin(int[] num) {
+        int min = num[0];
+        for (int i = 0; i < num.length; i++) {
+            if (min >= num[i]) {
+                min = num[i];
+            }
+        }//loop
+        System.out.println("The minimum value is " + min);
+    }
+
+
 
     private static void showData(int[] num) {
         for (int val : num) {
