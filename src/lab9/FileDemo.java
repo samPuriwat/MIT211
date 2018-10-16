9a8d1391acf996b021e4e2b7cba881c84c6515ad
samPuriwat <sam.puriwat@gmail.com>
2018-10-16 11:03:12 +0700

    Lab 9: File I/O

---
+++ src/lab9/FileDemo.java
@@ -0,0 +1,62 @@
+package lab9;
+
+import java.io.*;
+
+public class FileDemo {
+    public static void main(String[] args) {
+
+        try {
+
+            File myFile = new File("D:\\MIT211\\src\\lab9\\mit.txt");//mit.txt
+            if (myFile.createNewFile())
+                System.out.println("New File created.");
+            else
+                System.out.println("File already exited.");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        //write File
+            //writeMyFile();
+        //read file
+            readMyFile();
+
+    }//main
+
+    private static void readMyFile() {
+
+        try {
+            FileReader reader = new FileReader("D:\\MIT211\\src\\lab9\\mit.txt");
+            int i;
+            while ((i = reader.read()) != -1) {
+                    System.out.print((char) i);
+            }
+            reader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }//readMyFile
+
+    private static void writeMyFile() {
+        Writer w = null;
+        try {
+             w = new FileWriter("D:\\MIT211\\src\\lab9\\mit.txt",true);
+            String content = "MIT SAIYAI 2018 \r\n";
+            w.write(content);
+            System.out.println("Done.");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }finally {
+            try {
+                w.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }//writeMyFile
+}//class
